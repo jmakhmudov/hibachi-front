89c6a701b3cf09fd2ff7314adf1e33949db0821b
jmakhmudov <makhmudov@tuta.io>
2025-03-11 01:54:53 +0500

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import logo from '../../public/logo.webp'
 export default function Footer() {
   return (
     <footer className="bg-background mt-20 px-5 md:px-12 text-foreground py-5 w-screen">
-      <div className="flex flex-row justify-between items-start md:items-center gap-5">
+      <div className="flex flex-col justify-center items-center md:flex-row md:justify-between text-center md:text-right gap-5">
         <Link href='/' className='flex items-center gap-2 select-none'>
           <div className="bg-background rounded-full overflow-hidden">
             <Image src={logo} alt="logo" width={50} height={50} />
@@ -26,7 +26,7 @@ export default function Footer() {
           </ul>
 
 
-          <div className="mt-5 text-white/60 text-right">
+          <div className="mt-5 text-white/60 md:text-right">
             <p>Call us: <span className="font-semibold text-white/100">+1 (817) 6004955</span></p>
             <p>Email us: <span className="font-semibold text-white/100">contact@hibachibybamboo.com</span></p>
             <Link className=" mt-5 block text-main-red" href='https://www.instagram.com/Bamboo_hibachi/' target="_blank">Instagram</Link>
@@ -34,9 +34,9 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="text-sm flex flex-col md:flex-row justify-between md:items-center gap-5 mt-14">
+      <div className="text-sm flex flex-col md:flex-row justify-between md:items-center gap-5 mt-14 text-center md:text-right">
         <div className="opacity-50 order-2 md:order-1">All rights reserved. 2024 ©</div>
-        <div className="flex items-center gap-1 md:order-2">
+        <div className="flex items-center justify-center gap-1 md:order-2">
           <div className="opacity-50">developed by</div>
           <Link href="https://onegatestudios.com" target="_blank" className="font-bold block">ONEGATE.</Link>
         </div>
