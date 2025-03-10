81aa7ae76ae7e6fe55b6c55bb13340e20d696253
jmakhmudov <makhmudov@tuta.io>
2025-03-11 01:30:46 +0500

    add email and phone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,19 +17,28 @@ export default function Footer() {
           </div>
         </Link>
 
-        <ul className='font-medium flex flex-col md:flex-row md:items-center gap-2 md:gap-10 text-sm md:text-lg'>
-          <li>Locations</li>
-          <li>About Us</li>
-          <li>FAQ</li>
-          <li>Contact Us</li>
-        </ul>
+        <div>
+          <ul className='font-medium flex flex-col md:flex-row md:items-center gap-2 md:gap-10 text-sm md:text-lg'>
+            <li>Locations</li>
+            <li>About Us</li>
+            <li>FAQ</li>
+            <li>Contact Us</li>
+          </ul>
+
+
+          <div className="mt-5 text-white/60 text-right">
+            <p>Call us: <span className="font-semibold text-white/100">+1 (817) 6005955</span></p>
+            <p>Email us: <span className="font-semibold text-white/100">contact@hibachibybamboo.com</span></p>
+            <Link className=" mt-5 block text-main-red" href='https://www.instagram.com/Bamboo_hibachi/' target="_blank">Instagram</Link>
+          </div>
+        </div>
       </div>
 
       <div className="text-sm flex flex-col md:flex-row justify-between md:items-center gap-5 mt-14">
         <div className="opacity-50 order-2 md:order-1">All rights reserved. 2024 ©</div>
         <div className="flex items-center gap-1 md:order-2">
           <div className="opacity-50">developed by</div>
-          <div className="font-bold">ONEGATE.</div>
+          <Link href="https://onegatestudios.com" target="_blank" className="font-bold block">ONEGATE.</Link>
         </div>
       </div>
     </footer>
